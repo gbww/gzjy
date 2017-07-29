3c784da50590320a3dd491189caaea973dcb39d3
xbwangh <xbwangh@isoftstone.com>
2017-07-29 22:03:19 +0800

    test

--- src/main/java/com/gzjy/contract/controller/ContractController.java
+++ src/main/java/com/gzjy/contract/controller/ContractController.java
@@ -1,16 +1,12 @@
 package com.gzjy.contract.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gzjy.common.Response;
 import com.gzjy.contract.mapper.ContractMapper;
 import com.gzjy.contract.model.Contract;
 
@@ -21,28 +17,24 @@ public class ContractController {
 	ContractMapper contractMapper;
 
 	@RequestMapping(value = "/contract/{id}", method = RequestMethod.GET)
-	@ResponseBody
-	public String getContractById(@PathVariable(name="id") String id) {
+	public Response getContractById(@PathVariable String id) {
+		Contract contract = contractMapper.getById(id);
+		return Response.success(contract);
+	}
+	
+	@RequestMapping(value = "/contract/{id}", method = RequestMethod.DELETE)	
+	public Response delContractById(@PathVariable String id) {
+		Contract contract = contractMapper.getById(id);
+		return Response.success("success");
+	}
+	
+	@RequestMapping(value = "/contract/{id}", method = RequestMethod.PUT)	
+	public String updateContract(@PathVariable String id) {
 		Contract contract = contractMapper.getById("111");
 		return contract.toString();
 	}
 	
-	
-	@RequestMapping(value = "/contract/{id}", method = RequestMethod.DELETE)
-	@ResponseBody
-	public String delContractById(@PathVariable(name="id") String id) {
-		Contract contract = contractMapper.getById("111");
-		return contract.toString();
-	}
-	
-	@RequestMapping(value = "/contract/{id}", method = RequestMethod.PUT)
-	@ResponseBody
-	public String updateContract(@PathVariable(name="id") String id) {
-		Contract contract = contractMapper.getById("111");
-		return contract.toString();
-	}
-	
-	@RequestMapping(value = "/contracts", method = RequestMethod.DELETE)
+	/*@RequestMapping(value = "/contracts", method = RequestMethod.GET)
 	@ResponseBody
 	public JSONObject getPageList() {
 		HashMap<String, Object> result = new HashMap<String, Object>();
@@ -50,5 +42,5 @@ public class ContractController {
 		result.put("total", list.size());
 		result.put("list", list);		
 		return null;
-	}
+	}*/
 }
