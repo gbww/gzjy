15701691e51931649b203eaf20eeedfad4ae06af
xbwangh <xbwangh@isoftstone.com>
2017-08-01 21:16:44 +0800

    提交文件

--- src/main/java/com/gzjy/contract/mapper/ContractMapper.java
+++ src/main/java/com/gzjy/contract/mapper/ContractMapper.java
@@ -1,21 +1,19 @@
 package com.gzjy.contract.mapper;
 
-import java.util.ArrayList;
+import com.gzjy.contract.model.Contract;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import com.gzjy.contract.model.Contract;;
-
-@Mapper
 public interface ContractMapper {
+    int deleteByPrimaryKey(String id);
 
-	// @Select("select * from user where userName = #{name}")
-	Contract getById(@Param("id") String id);
+    int insert(Contract record);
 
-	ArrayList<Contract> getPageList(
-			@Param("id") String id, 
-			@Param("start") int start,
-			@Param("end") int end
-	);
+    int insertSelective(Contract record);
+
+    Contract selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Contract record);
+
+    int updateByPrimaryKeyWithBLOBs(Contract record);
+
+    int updateByPrimaryKey(Contract record);
 }
