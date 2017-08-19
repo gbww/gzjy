110638026a81491d174159e95beb86b1e36d4e36
xbwangh <xbwangh@isoftstone.com>
2017-08-19 12:11:08 +0800

    合同流程类
    Change-Id: I035071917f0a93e8166b6c14ab53fe9b806f42b9
    Signed-off-by: xbwangh <xbwangh@isoftstone.com>

---
+++ src/main/java/com/gzjy/contract/model/ContractProcess.java
@@ -0,0 +1,27 @@
+package com.gzjy.contract.model;
+
+import java.util.ArrayList;
+
+public class ContractProcess {
+	/*合同里面多个审批人*/
+	private ArrayList<String> approveUsers;
+	/*修改合同人*/
+	private String updateContractUser;
+
+	public ArrayList<String> getApproveUsers() {
+		return approveUsers;
+	}
+
+	public void setApproveUsers(ArrayList<String> approveUsers) {
+		this.approveUsers = approveUsers;
+	}
+
+	public String getUpdateContractUser() {
+		return updateContractUser;
+	}
+
+	public void setUpdateContractUser(String updateContractUser) {
+		this.updateContractUser = updateContractUser;
+	}
+
+}
