1eeea8597e4076bd041b706954b3d7d871108ab6
xbwangh <xbwangh@isoftstone.com>
2017-09-15 22:48:12 +0800

    添加流程引擎监听器
    Change-Id: I4369b85cd140b739bc7906992385455e6f082823
    Signed-off-by: xbwangh <xbwangh@isoftstone.com>

--- src/main/java/com/gzjy/contract/service/impl/TaskCompleteListener.java
+++ src/main/java/com/gzjy/contract/service/impl/TaskCompleteListener.java
@@ -1,13 +1,35 @@
 package com.gzjy.contract.service.impl;
 
-import org.activiti.engine.delegate.DelegateExecution;
-import org.activiti.engine.delegate.ExecutionListener;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class TaskCompleteListener implements ExecutionListener {
+import com.gzjy.contract.mapper.ContractMapper;
+import com.gzjy.contract.model.ContractStatus;
 
-	public void notify(DelegateExecution execution) throws Exception {
-		System.out.println("Task Listener");
+public class TaskCompleteListener implements TaskListener {
 
+	
+	@Autowired
+	private ContractMapper contractMapper;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;	
+
+	public void notify(DelegateTask delegateTask) {
+		
+		int resultCount = (int)delegateTask.getExecution().getVariable("resultCount");
+        int completeTask = (int)delegateTask.getExecution().getVariable("nrOfCompletedInstances");
+        int taskCount = (int)delegateTask.getExecution().getVariable("nrOfInstances");
+        System.out.println("Task Complete");
+        System.out.println("resultCount="+resultCount + "| completeTask="+completeTask);
+        String processId = delegateTask.getExecution().getProcessInstanceId();
+        if(completeTask > resultCount) {        	
+        	contractMapper.updateStatusByProcessId(ContractStatus.UPDATING.getValue(), processId);
+        }
+        if(taskCount == completeTask) {
+        	contractMapper.updateStatusByProcessId(ContractStatus.OVER.getValue(), processId);
+        }
 	}
-
 }
