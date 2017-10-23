55e1d0c7585f5cc84597478e99adb7c5f77f1367
Administrator <Administrator@PC-20171010DYEG>
2017-10-23 20:02:44 +0800

    添加excel转PDF测试文件
    Change-Id: I87b430f78f3029daf3dc2dfc388ca4863a70f548
    Signed-off-by: Administrator <Administrator@PC-20171010DYEG>

---
+++ src/test/java/wang/ExcelToPdf.java
@@ -0,0 +1,49 @@
+package wang;
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.ComThread;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
+
+public class ExcelToPdf {
+    public static void main(String args[]){
+        String inFilePath = "D:/template.xls";
+        String outFilePath = "D:/result1.pdf";
+        xlsToPdf(inFilePath, outFilePath);
+    }
+
+    public static void xlsToPdf(String inFilePath,String outFilePath){
+        ActiveXComponent ax = null;
+        Dispatch excel = null;
+        try {
+            ComThread.InitSTA();
+            ax = new ActiveXComponent("Excel.Application");
+            ax.setProperty("Visible", new Variant(false));
+            ax.setProperty("AutomationSecurity", new Variant(3)); // 禁用宏
+            Dispatch excels = ax.getProperty("Workbooks").toDispatch();
+            Object[] obj = new Object[]{
+                    inFilePath,
+                    new Variant(false),
+                    new Variant(false)
+            };
+            excel = Dispatch.invoke(excels, "Open", Dispatch.Method, obj, new int[9]).toDispatch();
+            // 转换格式
+            Object[] obj2 = new Object[]{
+                    new Variant(0), // PDF格式=0
+                    outFilePath,
+                    new Variant(0)  //0=标准 (生成的PDF图片不会变模糊) ; 1=最小文件
+            };
+            Dispatch.invoke(excel, "ExportAsFixedFormat", Dispatch.Method,obj2, new int[1]);
+        } catch (Exception es) {
+            es.printStackTrace();
+        } finally {
+            if (excel != null) {
+                Dispatch.call(excel, "Close", new Variant(false));
+            }
+            if (ax != null) {
+                ax.invoke("Quit", new Variant[] {});
+                ax = null;
+            }
+            ComThread.Release();
+        }
+    }
+}
