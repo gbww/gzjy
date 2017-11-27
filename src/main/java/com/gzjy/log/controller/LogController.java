bec5154e2aee92190006e6d0690abfd0a85da2ed
Administrator <Administrator@192.168.1.5>
2017-11-27 21:03:43 +0800

    commit

--- src/main/java/com/gzjy/log/controller/LogController.java
+++ src/main/java/com/gzjy/log/controller/LogController.java
@@ -1,6 +1,5 @@
 package com.gzjy.log.controller;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.pagehelper.PageInfo;
 import com.gzjy.common.Response;
 import com.gzjy.common.ShortUUID;
 import com.gzjy.log.constant.LogConstant;
@@ -52,6 +52,18 @@ public class LogController {
 		record.setCreateTime(new Date());
 		try {
 			List <LogModel> result = logService.selectAll(record);
+			return Response.success(result);
+		}
+		catch (Exception e) {
+			return Response.fail(e.getMessage());
+		}
+	}
+	
+	
+	@RequestMapping(value = "/log/page", method = RequestMethod.GET)
+	public Response getLogPageList() {
+		try {
+			PageInfo <LogModel> result = logService.getPageList(1, 10, "", "", "", "");
 			return Response.success(result);
 		}
 		catch (Exception e) {
