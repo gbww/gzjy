d9a5cfe704fa6425dda860cc6da689bd3742ac0d
bobowang2017 <409879654@qq.com>
2018-04-05 00:01:30 +0800

    提交合同流程引擎代码

---
+++ src/main/java/com/gzjy/receive/controller/ReportController.java
@@ -0,0 +1,147 @@
+package com.gzjy.receive.controller;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gzjy.common.Response;
+import com.gzjy.contract.model.ContractTask;
+import com.gzjy.receive.service.ReportService;
+
+@RestController
+@RequestMapping(value = "v1/ahgz/report")
+
+public class ReportController {
+	private static Logger logger = LoggerFactory.getLogger(ReportService.class);
+
+	/*@Autowired
+	private UserService userService;*/
+	@Autowired
+	private ReportService reportService;
+	
+	/**
+	 * 启动报告流程引擎接口
+	 * @param receiveSampleId
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/process", method = RequestMethod.POST)
+	public Response startProcess(@RequestParam(required = true) String receiveSampleId) throws Exception{
+		try {
+			reportService.deploymentProcess(receiveSampleId);
+			return Response.success("success");
+		} catch (Exception e) {
+			logger.error(e + "");
+			return Response.fail(e.getMessage());
+		}
+	}
+	
+	
+	/**
+	 * 查看当前用户任务列表
+	 * @param isHandle(0表示查看未完成任务，1 表示查看已完成任务)
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/process", method = RequestMethod.GET)
+	public Response getTaskByUser(@RequestParam(required = true, defaultValue="0") String isHandle) throws Exception{
+		try {
+			ArrayList<ContractTask> result = reportService.getContractTaskByUserId(isHandle);
+			return Response.success(result);
+		} catch (Exception e) {
+			logger.error(e + "");
+			return Response.fail(e.getMessage());
+		}
+	}
+	
+	
+	/**
+	 * 执行合同流程中编辑任务
+	 * @param taskId  任务编号
+	 * @param receiveSampleId 收样单编号
+	 * @param examinePersonId 审核人编号
+	 * @return
+	 */
+	@RequestMapping(value = "/process/task/edit/{taskId}", method = RequestMethod.GET)
+	public Response completeEditTask(@PathVariable String taskId,
+			@RequestParam(required = true) String receiveSampleId,
+			@RequestParam(required = true) String examinePersonId,
+			@RequestParam(required = false) String comment) {		
+		try {
+			reportService.doEditTask(taskId, receiveSampleId, examinePersonId, comment);
+			return Response.success("success");
+		} catch (Exception e) {
+			logger.error(e + "");
+			return Response.fail(e.getMessage());
+		}
+	}
+	
+	/**
+	 * 执行合同流程中审核任务
+	 * @param taskId  任务编号
+	 * @param receiveSampleId 收样单编号
+	 * @param approvePersonId 批准人编号
+	 * @return
+	 */
+	@RequestMapping(value = "/process/task/examine/{taskId}", method = RequestMethod.GET)
+	public Response completeExamineTask(@PathVariable String taskId,
+			@RequestParam(required = true) String receiveSampleId,
+			@RequestParam(required = true) String approvePersonId,
+			@RequestParam(required = true) boolean pass,
+			@RequestParam(required = false) String comment) {		
+		try {
+			reportService.doExamineTask(taskId, receiveSampleId, approvePersonId, pass, comment);
+			return Response.success("success");
+		} catch (Exception e) {
+			logger.error(e + "");
+			return Response.fail(e.getMessage());
+		}
+	}
+	
+	/**
+	 * 执行合同流程中批准任务
+	 * @param taskId  任务编号
+	 * @param receiveSampleId 收样单编号
+	 * @param printPersonId 打印人编号
+	 * @return
+	 */
+	@RequestMapping(value = "/process/task/approve/{taskId}", method = RequestMethod.GET)
+	public Response completeApproveTask(@PathVariable String taskId,
+			@RequestParam(required = true) String receiveSampleId,
+			@RequestParam(required = true) boolean pass,
+			@RequestParam(required = false) String comment) {		
+		try {
+			reportService.doApproveTask(taskId, receiveSampleId, pass, comment);
+			return Response.success("success");
+		} catch (Exception e) {
+			logger.error(e + "");
+			return Response.fail(e.getMessage());
+		}
+	}
+	
+	/**
+	 * 执行合同流程中打印任务
+	 * @param taskId  任务编号
+	 * @param receiveSampleId 收样单编号
+	 * @return
+	 *//*
+	@RequestMapping(value = "/process/task/print/{taskId}", method = RequestMethod.GET)
+	public Response completePrintTask(@PathVariable String taskId,
+			@RequestParam(required = true) String receiveSampleId,
+			@RequestParam(required = true) boolean pass) {		
+		try {
+			reportService.doPrintTask(taskId, receiveSampleId, pass);
+			return Response.success("success");
+		} catch (Exception e) {
+			logger.error(e + "");
+			return Response.fail(e.getMessage());
+		}
+	}*/
+}
---
+++ src/main/java/com/gzjy/receive/service/ReportService.java
@@ -0,0 +1,180 @@
+package com.gzjy.receive.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.task.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gzjy.contract.model.ContractTask;
+import com.gzjy.receive.mapper.ReceiveSampleMapper;
+import com.gzjy.receive.model.ReceiveSample;
+import com.gzjy.user.UserService;
+import com.gzjy.user.model.User;
+
+@Service
+public class ReportService {
+	private static Logger logger = LoggerFactory.getLogger(ReportService.class);
+	@Autowired
+	private ProcessEngine processEngine;
+	@Autowired
+	UserService userService;
+	@Autowired
+	ReceiveSampleMapper receiveSampleMapper;
+
+	/**
+	 * 启动报告流程引擎
+	 * @param receiveSampleId
+	 */
+	@Transactional
+	public void deploymentProcess(String receiveSampleId) {
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		User user = userService.getCurrentUser();
+		Map<String, Object> variables = new HashMap<String, Object>();
+		// 设定流程标记tag=1
+		variables.put("tag", 1);
+		// 启动流程引擎时首先要指定编辑人，默认是当前用户
+		variables.put("editPersion", user.getId());
+		String processId = runtimeService.startProcessInstanceByKey("ReportProcess", variables).getId();
+		// 流程启动成功之后将返回的流程ID回填到合同receive_sample表中
+		/*ReceiveSample receiveSample = new ReceiveSample();
+		receiveSample.setReceiveSampleId(receiveSampleId);
+		receiveSample.setReportStatus(1);
+		receiveSample.setReportProcessId(processId);
+		receiveSampleMapper.updateByPrimaryKey(receiveSample);*/
+	}
+
+	/**
+	 * 根据用户ID获取当前用户任务
+	 * @param isHandle(0表示查詢未完成任務，1表示查詢已完成任務)
+	 * @return ArrayList<ContractTask>
+	 */	
+	public ArrayList<ContractTask> getContractTaskByUserId(String isHandle) {
+		String userId = userService.getCurrentUser().getId();
+		ArrayList<ContractTask> taskList = new ArrayList<ContractTask>();
+		if ("0".equals(isHandle)) {
+			logger.info("查询未完成任务");
+			TaskService taskService = processEngine.getTaskService();
+			List<Task> tasks = taskService.createTaskQuery().taskAssignee(userId).list();
+			for (Task task : tasks) {
+				logger.info("ID:" + task.getId() + ",任务名称:" + task.getName() + ",执行人:" + task.getAssignee() + ",开始时间:"
+							+ task.getCreateTime());
+				ContractTask contractTask = new ContractTask();
+				contractTask.setId(task.getId());
+				contractTask.setName(task.getName());
+				contractTask.setAssignee(task.getAssignee());
+				contractTask.setCreateTime(task.getCreateTime());
+				contractTask.setProcessInstanceId(task.getProcessInstanceId());
+				contractTask.setExecutionId(task.getExecutionId());
+				taskList.add(contractTask);
+			}
+		} else {
+			HistoryService historyService = processEngine.getHistoryService();
+			List<HistoricTaskInstance> tasks = historyService.createHistoricTaskInstanceQuery().finished()
+					.taskAssignee(userId).list();
+			logger.info("历史任务:" + tasks.size() + " userId:" + userId);
+			for (HistoricTaskInstance task : tasks) {
+				logger.info("ID:" + task.getId() + ",任务名称:" + task.getName() + ",执行人:" + task.getAssignee() + ",开始时间:"
+						+ task.getCreateTime());
+				ContractTask contractTask = new ContractTask();
+				contractTask.setId(task.getId());
+				contractTask.setName(task.getName());
+				contractTask.setAssignee(task.getAssignee());
+				contractTask.setCreateTime(task.getCreateTime());
+				contractTask.setProcessInstanceId(task.getProcessInstanceId());
+				contractTask.setExecutionId(task.getExecutionId());
+				taskList.add(contractTask);
+			}
+		}
+		return taskList;
+	}
+
+	
+	/**
+	 * 执行报告中的编辑任务
+	 * @param taskId  任务编号
+	 */
+	public void doEditTask(String taskId, String receiveSampleId, String examinePersonId, String comment) {
+		TaskService taskService = processEngine.getTaskService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		Task task = (Task) taskService.createTaskQuery().taskId(taskId).list().get(0);
+		//执行任务过程中动态指定审核人
+		runtimeService.setVariable(task.getExecutionId(), "examinePersonId", examinePersonId);
+		//执行任务过程中需要设置标记位方便流程引擎区分
+		runtimeService.setVariable(task.getExecutionId(), "tag", 2);
+//		ReceiveSample receiveSample = receiveSampleMapper.selectByPrimaryKey(receiveSampleId);
+//		taskService.addComment(taskId,null, comment);
+		taskService.complete(taskId);		
+	}
+	
+	/**
+	 * 执行报告中的审核任务
+	 * @param taskId  任务编号
+	 */
+	@Transactional
+	public void doExamineTask(String taskId, String receiveSampleId,String approvePersonId, boolean pass, String comment) {
+		TaskService taskService = processEngine.getTaskService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		Task task = (Task) taskService.createTaskQuery().taskId(taskId).list().get(0);
+		//执行任务过程中可能同意可能拒绝,故需要设置标记位方便流程引擎区分
+		if (pass) {
+			runtimeService.setVariable(task.getExecutionId(), "tag", 3);
+			//执行任务过程中动态指定批准人
+			runtimeService.setVariable(task.getExecutionId(), "approvePersonId", approvePersonId);
+		}else {
+			runtimeService.setVariable(task.getExecutionId(), "tag", 1);
+		}
+//		taskService.addComment(taskId, null, comment);
+		taskService.complete(taskId);
+	}
+	
+	
+	/**
+	 * 执行报告中的批准任务
+	 * @param taskId  任务编号
+	 */
+	@Transactional
+	public void doApproveTask(String taskId, String receiveSampleId, boolean pass,String comment) {
+		TaskService taskService = processEngine.getTaskService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		Task task = (Task) taskService.createTaskQuery().taskId(taskId).list().get(0);
+		//执行任务过程中可能同意可能拒绝,故需要设置标记位方便流程引擎区分
+		if (pass) {
+			runtimeService.setVariable(task.getExecutionId(), "tag", 4);			
+		}else {
+			runtimeService.setVariable(task.getExecutionId(), "tag", 2);
+		}
+//		taskService.addComment(taskId, null, comment);
+		taskService.complete(taskId);
+	}
+	
+	
+	/**
+	 * 执行报告中的批准任务
+	 * @param taskId  任务编号
+	 *//*
+	@Transactional
+	public void doPrintTask(String taskId, String receiveSampleId,boolean pass) {
+		TaskService taskService = processEngine.getTaskService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		Task task = (Task) taskService.createTaskQuery().taskId(taskId).list().get(0);
+		//执行任务过程中可能同意可能拒绝,故需要设置标记位方便流程引擎区分
+		if (pass) {
+			runtimeService.setVariable(task.getExecutionId(), "tag", 5);			
+		}else {
+			runtimeService.setVariable(task.getExecutionId(), "tag", 3);
+		}
+		taskService.complete(taskId);
+	}*/
+}
