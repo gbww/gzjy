f513e338d26fae55b535503bc180d8c98c56472b
bobowang2017 <409879654@qq.com>
2018-04-27 23:29:02 +0800

    commit

--- src/main/java/com/gzjy/checkitems/controller/CheckItemsController.java
+++ src/main/java/com/gzjy/checkitems/controller/CheckItemsController.java
@@ -389,7 +389,6 @@ public class CheckItemsController {
 	 * @return
 	 */
 	@RequestMapping(value = "/checkitems/import", method = RequestMethod.POST)
-//	@Privileges(name = "CHECKITEM-IMPORT", scope = { 1 })
 	public Response multiImport(@RequestParam("file") MultipartFile file) {
 		try {
 			checkItemService.importFile(file);
