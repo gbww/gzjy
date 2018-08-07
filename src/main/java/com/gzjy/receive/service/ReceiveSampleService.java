9ee88559a74908bc194b34ad9d39f8e4c938409c
zyc1122 <zyc1122you@example.com>
2018-08-07 23:33:42 +0800

    修改了导出Excel的BUG

--- src/main/java/com/gzjy/receive/service/ReceiveSampleService.java
+++ src/main/java/com/gzjy/receive/service/ReceiveSampleService.java
@@ -359,15 +359,15 @@ public class ReceiveSampleService {
 		});
 		return pages;
 	}
-	public PageInfo<ReceiveSampleItem> writeExcel(Integer pageNum, Integer pageSize,String order,Map<String, Object> filter,OutputStream out) throws IOException {
-		/*User u=userClient.getCurrentUser();
+	public PageInfo<ReceiveSampleItem> writeExcel(Integer pageNum, Integer pageSize,String order,Map<String, Object> filter) throws IOException {
+		User u=userClient.getCurrentUser();
 		boolean superUser= u.getRole().isSuperAdmin();
 		if(!superUser) {
 			String name=u.getName();
 			filter.put("test_user", name);
-		}*/
-		filter.put("test_user", "王维");
-		filter.put("receivesample_status", "-1");
+		}
+		/*filter.put("test_user", "王维");
+		filter.put("receivesample_status", "-1");*/
 		List<ReceiveSampleItem> list = new ArrayList<ReceiveSampleItem>();
 		PageInfo<ReceiveSampleItem> pages = new PageInfo<ReceiveSampleItem>(list);
 		pages = PageHelper.startPage(pageNum, pageSize).doSelectPageInfo(new ISelect() {
